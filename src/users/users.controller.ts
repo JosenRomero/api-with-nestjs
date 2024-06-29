06fcf3ac7f012d5a5542567005c6754adc6c74e1
Jose <romerojosemanuel@hotmail.com>
2024-06-28 20:33:27 -0600

    feat: get an user by id in DB functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,16 @@
 import {
   Body,
   Controller,
+  Get,
+  HttpException,
+  Param,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/CreateUser.dto';
+import mongoose from 'mongoose';
 
 @Controller('users')
 export class UsersController {
@@ -16,5 +20,15 @@ export class UsersController {
   @UsePipes(new ValidationPipe())
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.createUser(createUserDto);
+  }
+
+  // users/:id
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const isValidID = mongoose.Types.ObjectId.isValid(id);
+    if (!isValidID) throw new HttpException('User not found', 404);
+    const findUser = await this.usersService.getUserById(id);
+    if (!findUser) throw new HttpException('User not found', 404);
+    return findUser;
   }
 }
