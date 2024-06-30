bc396d2e4b7fde666aeca82a828c52f73c6ef58f
Jose <romerojosemanuel@hotmail.com>
2024-06-30 16:55:12 -0600

    feat: add passport and jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,35 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/User.schema';
 import { CreateUserDto } from './dto/CreateUser.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwt: JwtService,
+  ) {}
 
   singup(createUserDto: CreateUserDto) {
     const newUser = new this.userModel(createUserDto);
-    return newUser.save();
+    newUser.save();
+    return this.signToken(newUser.username, newUser.email);
+  }
+
+  async signToken(
+    username: string,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: username,
+      email,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: process.env.JWT_SECRET,
+    });
+    return {
+      access_token: token,
+    };
   }
 }
