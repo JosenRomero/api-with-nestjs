f7e694c407db94a6788a90fe9858f7ecad6e7508
Jose <romerojosemanuel@hotmail.com>
2024-07-01 17:02:59 -0600

    remove code unnecessary

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,14 +5,12 @@ import {
   HttpException,
   Param,
   Patch,
-  Post,
   Req,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/CreateUser.dto';
 import mongoose from 'mongoose';
 import { UpdateUserDto } from './dto/UpdateUser.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -26,22 +24,6 @@ export class UsersController {
   @Get('me')
   getMe(@Req() req: Request) {
     return req.user;
-  }
-
-  @Post()
-  @UsePipes(new ValidationPipe())
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.createUser(createUserDto);
-  }
-
-  // users/:id
-  @Get(':id')
-  async getUserById(@Param('id') id: string) {
-    const isValidID = mongoose.Types.ObjectId.isValid(id);
-    if (!isValidID) throw new HttpException('User not found', 404);
-    const findUser = await this.usersService.getUserById(id);
-    if (!findUser) throw new HttpException('User not found', 404);
-    return findUser;
   }
 
   // users/:id
